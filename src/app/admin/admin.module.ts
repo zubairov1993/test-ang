4b68dd967056a08db853eaf13fa8ee2846fe9acf
vadim <vadim.zubairov1993@gmail.com>
2021-01-26 12:11:56 +0500

    'mod-log'

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,12 +7,15 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "./shared/services/auth.service";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {path: '', component: AdminLayoutComponent, children: [
         {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
@@ -24,7 +27,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     ])
   ],
   exports: [RouterModule],
-  declarations: [AdminLayoutComponent, DashbordPageComponent, LoginPageComponent, CreatePageComponent, EditPageComponent]
+  declarations: [AdminLayoutComponent, DashbordPageComponent, LoginPageComponent, CreatePageComponent, EditPageComponent],
+  providers: [
+    AuthService
+  ]
 })
 export class AdminModule {
 

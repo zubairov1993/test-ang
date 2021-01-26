f1f7a816aaf3c5a212aa571dc128a687ef1f6910
vadim <vadim.zubairov1993@gmail.com>
2021-01-26 12:35:44 +0500

    'mod-log'

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,12 +7,15 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "./shared/services/auth.service";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {path: '', component: AdminLayoutComponent, children: [
         {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
@@ -24,7 +27,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     ])
   ],
   exports: [RouterModule],
-  declarations: [AdminLayoutComponent, DashboardPageComponent, LoginPageComponent, CreatePageComponent, EditPageComponent]
+  declarations: [AdminLayoutComponent, DashboardPageComponent, LoginPageComponent, CreatePageComponent, EditPageComponent],
+  providers: [AuthService]
 })
 export class AdminModule {
 
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/shared/interfaces';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +13,10 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -29,5 +34,10 @@ export class LoginPageComponent implements OnInit {
       email: this.form.value.email,
       password: this.form.value.password
     }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }

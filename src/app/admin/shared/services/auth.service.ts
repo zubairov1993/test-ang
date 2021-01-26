e3271307853dd0792576275a61b78e699d7739b4
vadim <vadim.zubairov1993@gmail.com>
2021-01-26 12:56:22 +0500

    'add-fb'

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { User } from "src/app/shared/interfaces";
+import { tap } from "rxjs/operators";
+import { FbAuthResponse, User } from "src/app/shared/interfaces";
+import { environment } from "src/environments/environment";
 
 @Injectable()
 export class AuthService {
@@ -12,7 +14,9 @@ export class AuthService {
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user)
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user).pipe(
+      tap(this.setToken)
+    )
   }
 
   logout() {
@@ -23,7 +27,7 @@ export class AuthService {
     return !!this.token
   }
 
-  private setToken() {
-
+  private setToken(response: FbAuthResponse) {
+    console.log(response);
   }
 }

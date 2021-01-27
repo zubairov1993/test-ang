d80598c670cfc74b60699eee1a3877908723d009
vadim <vadim.zubairov1993@gmail.com>
2021-01-27 15:42:14 +0500

    'create-form'

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,10 +1,18 @@
-export class User {
+export interface User {
   email: string
   password: string
   returnSecureToken?: boolean
 }
 
-export class FbAuthResponse {
+export interface FbAuthResponse {
   idToken: string
   expiresIn: string
+}
+
+export interface Post {
+  id?: string
+  title: string
+  text: string
+  author: string
+  date: Date
 }

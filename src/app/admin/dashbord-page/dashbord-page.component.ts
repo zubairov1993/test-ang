dd8af8681812d6daf80cb02f47b74dcab70901c8
vadim <vadim.zubairov1993@gmail.com>
2021-01-28 11:58:15 +0500

    'add-posts-filter'

--- src/app/admin/dashbord-page/dashbord-page.component.ts
+++ src/app/admin/dashbord-page/dashbord-page.component.ts
@@ -12,6 +12,7 @@ export class DashbordPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSub: Subscription
+  searchStr = ''
 
   constructor(
     private postsService: PostsService

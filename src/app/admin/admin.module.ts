7cdc47c6cde196456344149e369bc34507283e29
vadim <vadim.zubairov1993@gmail.com>
2021-01-28 12:05:46 +0500

    'add-posts-filter'

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,6 +9,7 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
 import { AuthGuard } from "./shared/services/auth.guard";
+import { SearchPipe } from "./shared/search.pipe";
 
 @NgModule({
   imports: [
@@ -27,7 +28,14 @@ import { AuthGuard } from "./shared/services/auth.guard";
     ])
   ],
   exports: [RouterModule],
-  declarations: [AdminLayoutComponent, DashboardPageComponent, LoginPageComponent, CreatePageComponent, EditPageComponent],
+  declarations: [
+    AdminLayoutComponent,
+    DashboardPageComponent,
+    LoginPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    SearchPipe
+  ],
   providers: [AuthGuard]
 })
 export class AdminModule {

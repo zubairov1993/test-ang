7e7dda873d0f0c9efea1583d5d846091e889991e
vadim <vadim.zubairov1993@gmail.com>
2021-01-28 11:19:28 +0500

    'mod-dashboard'

--- src/app/admin/dashbord-page/dashbord-page.component.ts
+++ src/app/admin/dashbord-page/dashbord-page.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashbord-page',
   templateUrl: './dashbord-page.component.html',
   styleUrls: ['./dashbord-page.component.scss']
 })
-export class DashbordPageComponent implements OnInit {
+export class DashbordPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
+
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    const pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: string) {
+
   }
 
 }
